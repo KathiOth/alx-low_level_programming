d8a765c6e643d67959c4843fbdcb315520a638e4
KathiOth <kathiothman#gmail.com>
2023-10-16 16:28:38 -0700

    q

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,12 +15,12 @@ int _atoi(char *s)
 
 	do {
 		if (*s == '-')
-			signe *= -l;
+			signe *= -1;
 		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
 		else if (num > 0)
 			break;
 	} while (*s++);
 
-	return (num * sign);
+	return (num * signe);
 }
