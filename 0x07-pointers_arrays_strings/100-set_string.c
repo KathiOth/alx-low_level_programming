26f29105422246962a0286d229737675c7b6c83a
KathiOth <kathiothman#gmail.com>
2023-10-23 15:51:28 -0700

    q

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - set the value of a pointer to a char
+ * @s: pointer pointer
+ * @to: pointer chat
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
