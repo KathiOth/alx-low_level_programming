fb3555a88b771e8f92b0db16a7f1193cd7d03425
KathiOth <kathiothman#gmail.com>
2023-10-24 13:00:15 -0700

    q

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @c: first input
+ * @n: second input
+ * Return: sum of c and n
+*/
+
+int add(int c, int n)
+{
+	int sum = c + n;
+
+	return (sum);
+}
